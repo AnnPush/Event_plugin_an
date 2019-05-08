49a8cb8a4b5ec720c64481527ffc13a7ec28a4a7
AnnPush <32917082+AnnPush@users.noreply.github.com>
2019-05-08 23:19:04 +0300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,9 @@
+<?php
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+global $wpdb;
+ $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}eventplan");
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}newsplan");
 
