b543b6bce35129e98101698c17e355c67af45641
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-08-04 16:39:39 +0300

    Update p03_name.php

--- a1/p03_name.php
+++ a1/p03_name.php
@@ -2,3 +2,13 @@
 и фамилию в пе- ременной $last_name. Выведите символьную строку,
 содержащую имя и фамилию, разделив их пробелом. Кроме того, 
 выведите длину этой символьной строки. */
+
+<?php
+$first_name = 'Jack';
+$last_name = 'Nill';
+$str = "$first_name $last_name";
+
+//print 'Name: ' . $first_name . ' ' . $last_name;
+print 'Name: ' . $str . "\n";
+print 'Length: ' . strlen($str);
+?>
