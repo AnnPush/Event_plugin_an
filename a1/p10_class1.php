bc535d3ed0223e01c324aba606fd9d6c0eaa073d
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-08-05 17:47:09 +0300

    Update p10_class1.php

--- a1/p10_class1.php
+++ a1/p10_class1.php
@@ -1,3 +1,26 @@
 /*Создайте класс Ingredient. Каждый экземпляр этого класса должен представлять отдельный
 ингредиент блюда, а также отслеживать наименование ингредиента и его стоимость.
 */
+<?php
+
+class Ingredient
+{
+	protected $name;
+	protected $cost;
+
+	public function __construct($name, $cost)
+	{
+		$this -> name = $name;
+		$this -> cost = $cost;
+	}
+
+	public function getName()
+	{
+		return $this -> name;
+	}
+	public function getCost()
+	{
+		return $this -> cost;
+	}
+}
+?>
