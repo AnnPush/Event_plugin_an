a6e1510ab8f2ec0bc2510898b0e914d13a88f260
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-08-05 10:55:50 +0300

    Update p04_array.php

--- a1/p04_array.php
+++ a1/p04_array.php
@@ -15,5 +15,30 @@
 населении, а также общее население всех десяти городов.
 */
 
+<?php
+print "<table>\n";
+print "<tr><th>City</th><th>People</th></tr>\n";
+
+$enumeration = ['New York' => 8175133,
+'Los Angeles' => 3792621,
+'Chicago' => 2695598,
+'Houston' => 2100263,
+'Philadelphia' => 1526006,
+'Phoenix' => 1445632,
+'San Antonio' => 1327407,
+'San Diego' => 1307402,
+'Dallas' => 1197816,
+'San Jose' => 945942];
+
+$summa = 0;
+
+foreach($enumeration as $city =>$people)
+{
+	$summa += $people;
+	print "<tr><td>$city</td><td>$people</td></tr>\n";
+}
+print "<tr><td>Summa</td><td>$summa</td></tr>\n";
+print "</table>";
+?>
 
 
