1fed39e17d2367f5bec301f5050a2c69d2002a9e
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-08-05 14:44:09 +0300

    Update p07_fun1.php

--- a1/p07_fun1.php
+++ a1/p07_fun1.php
@@ -2,3 +2,23 @@
 функция должна принимать URL изображения в качестве обязательного аргумента, а так-
 же текст надписи, ширину и высоту изображения в качестве необязательных аргументов alt,
 height и width соответственно.*/
+<?php
+function f($url, $alt = null, $height = null, $wigth = null)
+{
+	$html = '<img src = "' . $url . '"';
+	if(isset($alt))
+	{
+		$html .= ' alt="' . $alt . '"';
+	}
+	if(isset($height))
+	{
+		$html .= ' height="' . $height . '"';
+	}
+	if(isset($width))
+	{
+		$html .= ' width="' . $width . '"';
+	}	
+}
+$html .= '/>';
+return $html;
+?>
