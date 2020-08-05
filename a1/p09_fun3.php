61ec2f9628b36f6dd19ed44bebed5b94b548ca07
AnnPush <32917082+AnnPush@users.noreply.github.com>
2020-08-05 15:01:41 +0300

    Update p09_fun3.php

--- a1/p09_fun3.php
+++ a1/p09_fun3.php
@@ -2,3 +2,13 @@
 один файл, загружающий первый файл, используя его для вывода на экран ряд дескрипторов
 <img />.
 */
+
+
+<?php
+
+include "f.php";
+$image_path = '/images/';
+print f('dog.png');
+print f('cat.png', 'ttt');
+print f('map.png',null,300,200);
+?>
